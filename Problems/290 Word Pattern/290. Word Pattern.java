b35a012b6060500c06fc50a21799f84e3c51d873
alvaro347 <alvaro347@gmail.com>
2024-11-15 20:10:34 +0000

    290 Word Pattern

--- Problems/290 Word Pattern/290. Word Pattern.java
+++ Problems/290 Word Pattern/290. Word Pattern.java
@@ -1,25 +1,31 @@
+import java.util.*;
+
 class Solution {
     public boolean wordPattern(String pattern, String s) {
         String[] split = s.split(" ");
 
         if (pattern.length() != split.length) {
-          return false;
+            return false;
         }
 
         HashMap<Character, String> map = new HashMap<>();
-        
+
         for (int i = 0; i < pattern.length(); i++) {
-          if (!map.get(pattern.charAt(i))) {
-            map.add(pattern.charAt(i), split[i]);
-          } else {
-            if (map.get(pattern.charAt(i)) != split[i]) {
-             return false;
+            if (!map.containsKey(pattern.charAt(i))) {
+                if (map.containsValue(split[i])) {
+                    return false;
+                } else {
+                    map.put(pattern.charAt(i), split[i]);
+
+                }
+            } else {
+                System.out.println("Value: " + map.get(pattern.charAt(i)) + " SplitS: " + split[i]);
+                if (!map.get(pattern.charAt(i)).equals(split[i])) {
+                    System.out.println(map.get(pattern.charAt(i)) + " " + split[i]);
+                    return false;
+                }
             }
-          }
-          
-        }
-        for (int j = 0; j < pattern.length(); j++) {
-          
+
         }
         return true;
     }
